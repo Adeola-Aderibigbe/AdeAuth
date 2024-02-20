60d7d9a420dabb08e6c5679e2217492ff3425da4
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-02-20 10:04:42 +0100

    Enable swagger in production

--- Program.cs
+++ Program.cs
@@ -77,11 +77,13 @@ builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    
+}*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
